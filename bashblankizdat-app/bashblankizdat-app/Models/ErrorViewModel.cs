cf16389b3105fbcde01a7fa31d2de47c55975d24
Diesel <92308937+Diesel-x@users.noreply.github.com>
2023-09-28 22:42:13 +0500

    Project created again

---
+++ bashblankizdat-app/bashblankizdat-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bashblankizdat_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
