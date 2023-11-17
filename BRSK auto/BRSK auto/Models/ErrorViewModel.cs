14e2ed80d0f8b149f700d9a27993a7b857300b40
Diesel <92308937+Diesel-x@users.noreply.github.com>
2023-11-17 23:33:19 +0500

    update

---
+++ BRSK auto/BRSK auto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BRSK_auto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
