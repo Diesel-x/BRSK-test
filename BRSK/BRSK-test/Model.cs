69ab1c8ccff256c2d99eb60efb897a642d2aabed
Diesel <92308937+Diesel-x@users.noreply.github.com>
2023-11-27 22:02:19 +0500

    Update

---
+++ BRSK/BRSK-test/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BRSK_test;
+
+public partial class Model
+{
+    public long Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool? Active { get; set; }
+
+    public long? IdBrand { get; set; }
+}
